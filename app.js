c61205d832c6db0a263a2940e2c9641f97c73bed
Anna Medyukh <annamedyukh@gmail.com>
2017-09-19 11:12:06 -0400

    up to nunjucks

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const app = express();
+const nj = require('nunjucks');
+
+let localVars = {
+    title: 'Twitter Clone',
+    users: [
+        {name: 'Kanye'},
+        {name: 'Gandalf'},
+        {name: 'Obama'}
+    ]
+};
+// nj.configure('views', {noCache: true});
+// nj.render('index.html', locals, function(err,output){
+//     if(err) throw error;
+//     console.log(output);
+// });
+
+app.set('views', 'html');
+app.engine('html', nj.render);
+nj.configure('views', {noCache: true});
+
 
 app.use('/', (req, res, next) => {
     console.log(req.method, req.originalURL, res.statusCode);
@@ -7,7 +27,13 @@ app.use('/', (req, res, next) => {
 })
 
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    // res.send("Hello World Meow");
+    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render('index', {title: 'Hall of Fame', people: people}, function(err,output){
+            if(err) throw error;
+            console.log(output);
+        });
+    
 });
 
 app.get('/news', function(req, res){
@@ -15,7 +41,8 @@ app.get('/news', function(req, res){
 });
 
 app.listen(3000, function(){
-    console.log('Listening on port 3000!')
+    console.log('Listening on port 3000!');
+    // console.log(rend);
 })
 
 
