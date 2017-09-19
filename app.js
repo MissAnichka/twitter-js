325c65451a1407b5fd8c5d1ca5b8714618b3817c
Anna Medyukh <annamedyukh@gmail.com>
2017-09-19 12:52:08 -0400

    front page working

--- app.js
+++ app.js
@@ -1,44 +1,31 @@
 const express = require('express');
 const app = express();
 const nj = require('nunjucks');
-
-// let localVars = {
-//     title: 'Twitter Clone',
-//     users: [
-//         {name: 'Kanye'},
-//         {name: 'Gandalf'},
-//         {name: 'Obama'}
-//     ]
-// };
-// nj.configure('views', {noCache: true});
-// nj.render('index.html', locals, function(err,output){
-//     if(err) throw error;
-//     console.log(output);
-// });
-
+const routes = require('./routes');
+app.use(express.static('public'))
+app.use('/', routes);
 app.set('view engine', 'html');
 app.engine('html', nj.render);
 nj.configure('views', {noCache: true});
 
 
-app.use('/', (req, res, next) => {
-    console.log(req.method, req.originalURL, res.statusCode);
-    next();
-})
+// app.use('/', (req, res, next) => {
+//     console.log(req.method, req.originalURL, res.statusCode);
+//     next();
+// })
 
-app.get('/', function(req, res){
-    // res.send("Hello World Meow");
-    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-    res.render('index', {title: 'Hall of Fame', people: people});
-});
+// app.get('/', function(req, res){
+//     // res.send("Hello World Meow");
+//     const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//     res.render('index', {title: 'Hall of Fame', people: people});
+// });
 
-app.get('/news', function(req, res){
-	res.send('news page');
-});
+// app.get('/news', function(req, res){
+// 	res.send('news page');
+// });
 
 app.listen(3000, function(){
     console.log('Listening on port 3000!');
-    // console.log(rend);
 })
 
 
