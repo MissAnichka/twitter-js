8857d20f4c82385f209416462fa52fefe878e5f4
Anna Medyukh <annamedyukh@gmail.com>
2017-09-19 11:21:16 -0400

    working bare-bones template with nunjucks

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 const nj = require('nunjucks');
 
-let localVars = {
-    title: 'Twitter Clone',
-    users: [
-        {name: 'Kanye'},
-        {name: 'Gandalf'},
-        {name: 'Obama'}
-    ]
-};
+// let localVars = {
+//     title: 'Twitter Clone',
+//     users: [
+//         {name: 'Kanye'},
+//         {name: 'Gandalf'},
+//         {name: 'Obama'}
+//     ]
+// };
 // nj.configure('views', {noCache: true});
 // nj.render('index.html', locals, function(err,output){
 //     if(err) throw error;
 //     console.log(output);
 // });
 
-app.set('views', 'html');
+app.set('view engine', 'html');
 app.engine('html', nj.render);
 nj.configure('views', {noCache: true});
 
@@ -29,11 +29,7 @@ app.use('/', (req, res, next) => {
 app.get('/', function(req, res){
     // res.send("Hello World Meow");
     const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-    res.render('index', {title: 'Hall of Fame', people: people}, function(err,output){
-            if(err) throw error;
-            console.log(output);
-        });
-    
+    res.render('index', {title: 'Hall of Fame', people: people});
 });
 
 app.get('/news', function(req, res){
